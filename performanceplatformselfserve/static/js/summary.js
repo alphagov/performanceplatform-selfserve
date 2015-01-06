3a2869d02d781ae142236c9225e0d1703fa00104
Jon Wyatt <jonnywyatt@yahoo.co.uk>
2015-01-06 14:43:33 +0000

    Summary - add cancel buttons. Move 'anything else' after table. Link preview

--- performanceplatformselfserve/static/js/summary.js
+++ performanceplatformselfserve/static/js/summary.js
@@ -1,28 +1,38 @@
 $('.js-change').on('click', function () {
-  $(this)
-    .toggleClass('is-active')
-    .html(function(index, oldHtml) {
-      return ($.trim(oldHtml) === 'Change' ? 'Save' : 'Change');
-    })
-    .closest('.js-edit-group')
-    .toggleClass('is-not-editing')
-    .find('.form-control, .js-selectable')
-    .prop('disabled', function () {
-      return ! $(this).prop('disabled');
-    })
-    .first().focus();
+    toggleEditMode($(this));
 });
 
+$('.js-cancel').on('click', function() {
+    toggleEditMode($(this).prev('.js-change'));
+});
+
+function toggleEditMode($changeBtn) {
+    $changeBtn
+        .toggleClass('is-active')
+        .html(function (index, oldHtml) {
+            return ($.trim(oldHtml) === 'Change' ? 'Save' : 'Change');
+        })
+        .next('.js-cancel')
+        .toggleClass('hidden')
+        .closest('.js-edit-group')
+        .toggleClass('is-not-editing')
+        .find('.form-control, .js-selectable')
+        .prop('disabled', function () {
+            return !$(this).prop('disabled');
+        })
+        .first().focus();
+}
+
 $('.js-selectable').on('click', function () {
-  var isRadio = $(this).attr('type') === 'radio',
-    $selectedLabel;
-  $selectedLabel = $(this).closest('label');
+    var isRadio = $(this).attr('type') === 'radio',
+        $selectedLabel;
+    $selectedLabel = $(this).closest('label');
 
-  $selectedLabel.toggleClass('is-selected');
+    $selectedLabel.toggleClass('is-selected');
 
-  if (isRadio) {
-    $selectedLabel.siblings('label').removeClass('is-selected');
-  }
+    if (isRadio) {
+        $selectedLabel.siblings('label').removeClass('is-selected');
+    }
 });
 
 $('textarea').autosize();
